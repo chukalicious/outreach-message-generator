09e84e7b7fcbd247594e23a5a916cf3f61315aa5
Katiuska Alicea <katiuskaalicea@gmail.com>
2023-01-06 21:04:59 -0500

    quick push on main because I started a feature without creating a new branch for it. will push, create branch and continue

--- src/components/messages/HiringManager/HiringManager.js
+++ src/components/messages/HiringManager/HiringManager.js
@@ -1,4 +1,14 @@
+import { useState, useEffect } from "react";
+
 const HiringManager = () => {
+  const [input, setInput] = useState({
+    goodFit1: "",
+    goodFit2: "",
+    skill1: "",
+    skill2: "",
+    skill3: "",
+  });
+  console.log("HiringManager: input: ", input);
   return <>hiring manager</>;
 };
 
