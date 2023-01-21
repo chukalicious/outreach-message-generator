67354d0181eb7232f80338e3f4c76bda9baeae1f
Katiuska Alicea <katiuskaalicea@gmail.com>
2023-01-21 01:40:32 -0800

    found email valadation API that actually works and implemented it, also added the .env.local file to the gitignore

--- src/components/EmailValidator/EmailValidatorContainer.js
+++ src/components/EmailValidator/EmailValidatorContainer.js
@@ -1,7 +1,30 @@
 import EmailValidator from "./EmailValidator";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const EmailValidatorContainer = () => {
-  return <EmailValidator />;
+  const [emailAddress, setEmailAddress] = useState("");
+  const [deliverable, setDeliverable] = useState();
+  console.log("Email Address", emailAddress);
+
+  const getEmailAddress = (address) => {
+    setEmailAddress(address.email);
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://emailvalidation.abstractapi.com/v1/?api_key=df3b9c21381f428f8ce52f4cbebdf0df&email=${emailAddress}`
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [emailAddress]);
+
+  return <EmailValidator getEmailAddress={getEmailAddress} />;
 };
 
 export default EmailValidatorContainer;
